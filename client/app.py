8f366862eac669df106ce600bb77137e356bee7a
Simon Diemert <simon.diemert@gmail.com>
2015-08-12 21:14:43 -0700

    setup basic UI

--- client/app.py
+++ client/app.py
@@ -1,14 +1,53 @@
 import wx
 
+
 class MyFrame(wx.Frame):
-
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(200, 100))
+        wx.Frame.__init__(self, parent, title=title, size=(800, 600))
 
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.SetMaxSize(wx.Size(800, 600))
 
         self.display = wx.Panel(self, wx.ID_ANY)
-        self.static = wx.StaticText(self.display, wx.ID_ANY, "static text")
+        # self.bottomPanel = wx.Panel(self, wx.ID_ANY)
+
+        self.title = wx.StaticText(self.display, wx.ID_ANY, "Clicker Client")
+        self.divider = wx.StaticLine(self.display, wx.ID_ANY)
+        self.okButton = wx.Button(self.display, wx.ID_ANY, "Go!")
+        self.input1 = wx.ComboBox(self.display, wx.ID_ANY, value="Select Source", choices=['a', 'b', 'c'], size=(200, -1))
+        self.output = wx.TextCtrl(self.display, wx.ID_ANY, size=(800, 400), style=wx.TE_MULTILINE)
+
+        self.title.SetFont(wx.Font(18, wx.DECORATIVE, wx.ITALIC, wx.NORMAL))
+        self.output.SetEditable(False)
+
+        wrapper = wx.BoxSizer(wx.VERTICAL)
+        top = wx.BoxSizer(wx.HORIZONTAL)
+        content = wx.BoxSizer(wx.VERTICAL)
+        control = wx.BoxSizer(wx.HORIZONTAL)
+        feedback = wx.BoxSizer(wx.HORIZONTAL)
+        bottom = wx.BoxSizer(wx.HORIZONTAL)
+
+        top.Add(self.title, 0, wx.ALL, 5)
+
+        # controls the height of the top bar.
+        top.SetMinSize(wx.Size(-1, -1))
+
+        control.Add(self.input1, 0, wx.ALL, 5)
+        control.Add(self.okButton, 0, wx.ALL, 5)
+
+        feedback.Add(self.output, 0, wx.ALL, 5)
+
+        content.Add(control, 0, wx.ALL, 5)
+        content.Add(feedback, 0, wx.ALL, 5)
+
+        wrapper.Add(top, 0, wx.ALL, 5)
+        wrapper.Add(self.divider, 0, wx.ALL|wx.EXPAND, 5)
+        wrapper.Add(content, 0, wx.ALL, 5)
+        wrapper.Add(bottom, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.display.SetSizer(wrapper)
+
+        wrapper.Fit(self)
+
         self.CreateStatusBar()
 
         filemenu = wx.Menu()
@@ -20,7 +59,7 @@ class MyFrame(wx.Frame):
 
         other = filemenu.Append(1010, 'foobar', "does a foobar")
         self.Bind(wx.EVT_MENU, self.OnFooBar, other)
-        
+
         menubar = wx.MenuBar()
         menubar.Append(filemenu, "&File")
 
@@ -32,6 +71,6 @@ class MyFrame(wx.Frame):
         print "foobar!"
 
 
-app = wx.App(False) #don't redirect stdout/stderr, create new app.
-frame = MyFrame(None, "Small Editor") 
+app = wx.App(False)  # don't redirect stdout/stderr, create new app.
+frame = MyFrame(None, "Clicker Uploader")
 app.MainLoop()
