a95bb375a00446bbaf8bd98484459f41929b8ece
Simon Diemert <simon.diemert@gmail.com>
2015-08-30 17:20:37 -0700

    added readme to webapp

---
+++ webapp/README.md
@@ -0,0 +1,12 @@
+# Clicker - Web Application
+
+This document describes the web application component of the [http://github.com/sdiemert/clicker](Clicker Project). The web application is a Node Express based application meant to be run in a Unix Environment. It stores data in a Mongo Database. 
+
+## Structure
+
+## Dependencies
+
+All dependencies can be installed by running `npm install` win the web applications project root directory (`clicker/webapp`). Specific dependencies are:
+
+* Express - web service framework.
+* Mongoose - ODM library for interacting with MongoDB.
--- webapp/routes/members.js
+++ webapp/routes/members.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var router  = express.Router();
 
+var Member = require("../lib/models/Member").Member;
+
 var members = [
     {memberId: "scienceventure", memberName: "Science Venture"},
     {memberId: "scienceventure", memberName: "Science Venture"}
 ];
 
+/**
+ * Returns a list of members and their associated meta-data.
+ */
 router.get('/', function (req, res, next) {
 
     res.status(200);
     return res.json(members);
 
-
 });
 
+
+/**
+ * Renders a list
+ */
 router.get('/:name', function (req, res, next) {
     res.render('member', {});
 });
